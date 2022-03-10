9cfdc85ea9c11258b00e42a5f899d44260a9cb31
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-03-10 14:24:52 -0600

    configure for kovan deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 require('babel-register');
 require('babel-polyfill');
 
+const path = require("path");
+const fs = require("fs")
+const provider = require("@truffle/hdwallet-provider")
+const secrets = JSON.parse(fs.readFileSync('.secrets.json').toString().trim())
+
 module.exports = {
   networks: {
     development: {
@@ -8,6 +13,14 @@ module.exports = {
       port: 9545,
       network_id: "*" // Match any network id
     },
+    kovan: {
+      provider: () => 
+        new provider(secrets.privateKey, secrets.infuraApi,
+          0,
+          1
+        ),
+      network_id: 42
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
